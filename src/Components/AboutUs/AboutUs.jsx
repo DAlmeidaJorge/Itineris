230a5ca9902fe507144005446160e24141ea83e8
DAlmeidaJorge <duartejorge_93@hotmail.com>
2023-05-18 13:00:09 +0100

    almost finished about us section

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -5,17 +5,33 @@ const AboutUs = () => {
   return (
 
 
-
+      
     <div className='about-us-main'>
-        <div className='about-us-text-container'>
-            <h2>About Us</h2>
-            <p>My name is Eduardo Jorge and I am am now a free wildling</p>
-            <button>Give me money</button>
-        </div>
-        <img className="about-us-image" src="https://secretcompass.com/wp-content/uploads/2018/01/Jungle-Rot.jpg" alt="jungle" width={"450px"}/>
+            
+     <div className='about-us-text-container'>
+                 <h2 className='who-title'>WHO WE ARE</h2>
+                 <h2 className='about-title'>About Us</h2>
+            <p className='about-text'>Discover the world with Itineris! We're your ultimate travel companion, offering curated destinations, captivating galleries,and authentic traveler testimonials. Plan your adventure,
+                 find inspiration, and unlock unforgettable experiences. Join our passionate community of wanderers and start exploring today!</p>
+                 <div className='about-statistics-container'>
+                    <h2 className='numbers'>45</h2>
+                    <h2 className='about-description'>Type of Activities</h2>
+                    <h2 className='numbers'>35</h2>
+                    <h2 className='about-description'>Countries Available</h2>
+                    <h2 className='numbers'>80</h2>
+                    <h2 className='about-description'>Traveling Testimonials</h2>
+                    <h2 className='numbers'>+100</h2>
+                    <h2 className='about-description'>Photographs</h2>
+                 </div>
+            <button className='contact-button'>Contact Us</button>
+                 </div>
+         <img className='about-cover' src ="https://media.istockphoto.com/photos/travel-planning-concept-on-map-picture-id891573112?b=1&k=20&m=891573112&s=612x612&w=0&h=cEVoMlFS_LmuQVlRthh3-tE4d_iZacnXXT8WvtFrddg=" alt="map-img" width={"25%"} height={"25%"} opacity={"90%"} /> 
+                 </div>
+        
+
     
      
-      </div>
+      
 )
 }
 
